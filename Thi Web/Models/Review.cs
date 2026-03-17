a8352dcf356affeaec786839b3e16b0e6592e7f6
tamle10 <TAM@DESKTOP-RVH04FS>
2026-03-17 19:14:16 +0700

    Tạo mới tệp Models/Review.cs

---
+++ Thi Web/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechShop.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser? User { get; set; }
+
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
