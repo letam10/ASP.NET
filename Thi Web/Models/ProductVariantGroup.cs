355f667e5fe8e63e6cc94dfb545fe056f429f210
letam10 <ledominhtam1998@gmail.com>
2026-03-24 00:20:58 +0700

    Fix 3

---
+++ Thi Web/Models/ProductVariantGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace TechShop.Models
+{
+    public class ProductVariantGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; // Màu sắc, RAM, Ổ cứng...
+        public List<ProductVariantOption> Options { get; set; } = new();
+    }
+}
