a754cb7e2ed2fc4b14aad2b6def841b381f11048
letam10 <ledominhtam1998@gmail.com>
2026-03-24 00:09:16 +0700

    Create AdminDashboardViewModel.cs

---
+++ Thi Web/ViewModels/Admin/AdminDashboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TechShop.ViewModels.Admin
+{
+    public class AdminDashboardViewModel
+    {
+        public decimal TotalRevenue { get; set; }
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        public List<TopProductItemViewModel> TopProducts { get; set; } = new();
+        public List<SalesRatioItemViewModel> SalesRatio { get; set; } = new();
+    }
+    public class TopProductItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int SoldQuantity { get; set; }
+        public decimal Revenue { get; set; }
+    }
+    public class SalesRatioItemViewModel
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int SoldQuantity { get; set; }
+        public double Percentage { get; set; }
+    }
+}
